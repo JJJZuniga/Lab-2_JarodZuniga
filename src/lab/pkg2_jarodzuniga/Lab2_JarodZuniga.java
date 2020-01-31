14c3b3d57e83564f20b71fc28439ab29001e1332
Rosalba <JJJZuniga>
2020-01-31 13:26:10 -0600

    Mi laboratorio numero 2

---
+++ src/lab/pkg2_jarodzuniga/Lab2_JarodZuniga.java
@@ -0,0 +1,20 @@
+
+package lab.pkg2_jarodzuniga;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Lab2_JarodZuniga {
+
+    static Scanner sc=new Scanner(System.in);
+    static Random rn=new Random();
+    public static void main(String[] args) {
+        
+        System.out.println("Bienvenido a juego codest");
+        System.out.println("|---------------------*MENU*----------------------|");
+        System.out.println("|-------------------------------------------------|");
+        System.out.println("|1. ");
+    }
+    
+}
